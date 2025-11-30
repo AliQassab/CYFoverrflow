11f5e7ffbfa5ae859f3146984dcdf2531e00caec
Alaa <aliqassabuk@gmail.com>
2025-11-30 21:36:26 +0000

    Add health code check

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,31 @@ import { connectDb } from "./db.js";
 import config from "./utils/config.js";
 import logger from "./utils/logger.js";
 
-const { port } = config.init();
-
-// Try to connect to database, but don't crash if it fails
-// The health check will report the status
 try {
-	await connectDb();
+  const { port } = config.init();
+
+  // Try to connect to database, but don't crash if it fails
+  try {
+    await connectDb();
+  } catch (error) {
+    logger.error("Failed to connect to database on startup: %O", error);
+    logger.warn(
+      "Server will start, but database operations will fail until connection is established"
+    );
+  }
+
+  const { default: app } = await import("./app.js");
+
+  // Listen on 0.0.0.0 to accept connections from outside the container
+  const server = app.listen(port, "0.0.0.0", () => {
+    logger.info(`listening on 0.0.0.0:${port}`);
+  });
+
+  server.on("error", (err) => {
+    logger.error("Server error: %O", err);
+    process.exit(1);
+  });
 } catch (error) {
-	logger.error("Failed to connect to database on startup: %O", error);
-	logger.warn("Server will start, but database operations will fail until connection is established");
+  console.error("Fatal error during server startup:", error);
+  process.exit(1);
 }
-
-const { default: app } = await import("./app.js");
-
-app.listen(port, (err) => {
-	if (err) {
-		throw err;
-	}
-	logger.info(`listening on ${port}`);
-});
